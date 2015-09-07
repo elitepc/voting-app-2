b8ab922aad92aa906bc92ec96fcb6a0ed2f22e8a
elitepc <elite_pc_@hotmail.com>
2015-09-07 10:35:20 +0100

    added item to navbar

--- client/app/poll/poll.controller.js
+++ client/app/poll/poll.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('votingAppApp')
-  .controller('PollCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('PollCtrl', function ($scope, $http, Auth, socket) {
+    $scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+
+
   });
