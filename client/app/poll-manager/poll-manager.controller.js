4295908263a40736f8132c1969975b93854724ed
elitepc <elite_pc_@hotmail.com>
2015-09-07 04:53:31 +0100

    added user polls visible in poll-manager

--- client/app/poll-manager/poll-manager.controller.js
+++ client/app/poll-manager/poll-manager.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('votingAppApp')
-  .controller('PollManagerCtrl', function ($scope) {
+  .controller('PollManagerCtrl', function ($scope, $http, Auth, socket) {
     $scope.message = 'Hello';
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $http.get('/api/polls/mypolls').success(function(awesomeThings) {
+      $scope.myPolls = awesomeThings;
+      socket.syncUpdates('poll', $scope.myPolls);
+    });
+
+    $scope.addPoll = function() {
+      if($scope.newPollName === '') {
+        return;
+      }
+      $http.post('/api/polls', { name: $scope.newPollName });
+      $scope.newThing = '';
+    };
   });
